7b41a79cd966b472ddf3dcbf84d7bba48683135d
Kidney-02 <84661204+Kidney-02@users.noreply.github.com>
2024-12-08 15:34:21 +0100

    Updated Print Val Button to use array instead of single value

--- content/scenes/Print_Var.gd
+++ content/scenes/Print_Var.gd
@@ -1,7 +1,13 @@
 extends Button
-@export var variable_name = "button_val"
+#@export var variable_name = "button_val"
+@export var variable_names = [1]
+
 
 func _on_pressed():
 	#print(GlobalVariables.get("number"))
-	print(GlobalVariables.get(variable_name))
+	
+	for n in variable_names:
+		print(GlobalVariables.get(n))
+	
+	
 	pass # Replace with function body.
